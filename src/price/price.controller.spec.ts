6d0e83e5018cc794d1a8eb0042e7475c6065af8e
Vladimir Lastovka <elcopro@gmail.com>
2023-03-25 15:08:21 +0300

    price controller

--- src/price/price.controller.spec.ts
+++ src/price/price.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PriceController } from './price.controller';
+import { PriceService } from './price.service';
+import { GoodDto } from '../good/dtos/good.dto';
 
 describe('PriceController', () => {
     let controller: PriceController;
@@ -7,6 +9,14 @@ describe('PriceController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [PriceController],
+            providers: [
+                {
+                    provide: PriceService,
+                    useValue: {
+                        getPrices: async () => [new GoodDto()],
+                    },
+                },
+            ],
         }).compile();
 
         controller = module.get<PriceController>(PriceController);
@@ -14,5 +24,10 @@ describe('PriceController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('get prices', () => {
+        const response = controller.findAll({ search: '123', dbOnly: false, withCache: true });
+        expect(response).resolves.toStrictEqual([new GoodDto()]);
     });
 });
