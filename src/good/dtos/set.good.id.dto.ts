fb21632d87aa8a6bba874b87c4ca4dfa8ba68bb3
Vladimir Lastovka <elcopro@gmail.com>
2023-05-12 19:37:26 +0300

    swagger goodId

--- src/good/dtos/set.good.id.dto.ts
+++ src/good/dtos/set.good.id.dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SetGoodIdDto {
+    @ApiProperty({ description: 'Supplier alias', example: 'elcopro' })
     @IsNotEmpty()
     supplierAlias: string;
 
+    @ApiProperty({ description: 'Supplier Good Id', example: '1234567890' })
     @IsNotEmpty()
     supplierGoodId: string;
 
+    @ApiProperty({ description: 'Good Id', example: '3dc5e1cc1726a755a87eed94a4862228' })
     @IsNotEmpty()
     goodId: string;
 }
--- src/good/good.controller.ts
+++ src/good/good.controller.ts
@@ -1,10 +1,25 @@
 import { Controller, Post, Query } from '@nestjs/common';
 import { SetGoodIdDto } from './dtos/set.good.id.dto';
 import { GoodService } from './good.service';
-
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+@ApiTags('good')
 @Controller('good')
 export class GoodController {
     constructor(private goodService: GoodService) {}
+    @ApiCreatedResponse({
+        schema: {
+            type: 'object',
+            properties: {
+                result: {
+                    type: 'boolean',
+                    example: false,
+                },
+            },
+        },
+        description:
+            'Bind the id of the supplier of the goods to the good of the database. ' +
+            'True - success, False - problem with supplierAlias or goodId',
+    })
     @Post('good-id')
     async setGoodId(@Query() setGoodDto: SetGoodIdDto): Promise<any> {
         const result = await this.goodService.setGood(setGoodDto);
